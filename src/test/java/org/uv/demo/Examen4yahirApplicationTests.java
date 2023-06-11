51931059772074add47defbcc14b0883a0543ac1
YACRUZ <yamatsukino@gmail.com>
2023-06-11 15:04:46 -0600

    Update test

--- src/test/java/org/uv/demo/Examen4yahirApplicationTests.java
+++ src/test/java/org/uv/demo/Examen4yahirApplicationTests.java
@@ -1,24 +1,24 @@
 package org.uv.demo;
 
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest (classes = Examen4yahirApplication.class)
+@SpringBootTest(classes = Examen4yahirApplication.class)
 class Examen4yahirApplicationTests {
 
-	@Test
-    public void miMetodoTest() {
+    @Test
+    void contextLoads() {
         // Lógica de prueba
-        int resultado=5;
-        resultado = miMetodo();
+        boolean resultado = myMethod();
 
         // Afirmación
-        
+        assertTrue(resultado);
     }
 
-    public int miMetodo() {
-        return 5; // El resultado esperado es 5
+    public boolean myMethod() {
+        // Lógica del método que estás probando
+        return true; // El resultado esperado es verdadero (true)
     }
+
 }
-
