d872b39d3aca61bd60fa91c15d8f4175e19e68ad
YACRUZ <yamatsukino@gmail.com>
2023-06-11 14:47:06 -0600

    Update test

--- src/test/java/org/uv/demo/Examen4yahirApplicationTests.java
+++ src/test/java/org/uv/demo/Examen4yahirApplicationTests.java
@@ -3,7 +3,7 @@ package org.uv.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = Examen4yahirApplication.class)
 class Examen4yahirApplicationTests {
 
 	@Test
